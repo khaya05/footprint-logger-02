46d99d960c319cee3343f018c41f32b30aecfe0a
Khaya Mnyandu <88523281+khaya05@users.noreply.github.com>
2025-09-04 19:03:22 +0000

    bug fix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express()
 
-import userRouter from './routes/userRouter'
+import userRouter from './routes/userRouter.js'
 
 
 app.use(express.json())
@@ -10,9 +10,9 @@ app.use('/api/v1/users', userRouter)
 const port = process.env.PORT || 5100
 
 try {
-  app.listen(port, () =>{
+  app.listen(port, () => {
     console.log(`App running on port ${port}`);
-    
+
   })
 } catch (error) {
   console.error(error)
