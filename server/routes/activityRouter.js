e58c30404e2e796360c3756d8b75584dd5cc26eb
khaya05 <khayamnyandu5@gmail.com>
2025-09-05 13:17:01 +0200

    rm .env

--- server/routes/activityRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createActivity, deleteActivity, getActivity, getAllActivities, updateActivity } from "../controllers/activityController.js";
-const router = Router()
-
-router.route('/').get(getAllActivities).post(createActivity)
-router.route('/:id').get(getActivity).patch(updateActivity).delete(deleteActivity)
-
-export default router
